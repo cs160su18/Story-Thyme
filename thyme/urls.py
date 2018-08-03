63e78df7a314789a341fd180ee95f3e3762a41ee
xperrylinn <xperrylinn@berkeley.edu>
2018-08-03 16:52:22 -0400

    Added code to views, urls, and homepage.html to process searching for timeline. Still need to test. XPL.

--- thyme/urls.py
+++ thyme/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('homepage/', views.homepage, name='homepage'),
+    path('homepageSearchQuery/', views.homepageSearchQuery, name='homepageSearchQuery'),
 ]
--- thyme/views.py
+++ thyme/views.py
@@ -10,3 +10,29 @@ def index(request):
   
 def homepage(request):
   return render(request, 'thyme/homepage.html')
+
+# Process search for Timeline from homepage.html
+def homepageSearchQuery(request):
+    queryDishName = request.GET.get('dishName', None)
+    dishNameExists = Timeline.objects.filter(dishName=queryDishName).exists()
+    if dishNameExists:
+      timeslines = Timeline.objects.filter(dishName=queryDishName)
+      obj = {}
+      counter = 0
+      for timeline in timelines:
+          obj["timeline" + str(counter)] = helper(timeline)
+          counter = counter + 1
+      return JsonResponse(obj)
+    else:
+      # Timeline doesn't exist
+      data = {
+        'success': 'timeline_error'
+      }
+      return JsonResponse(data) 
+  
+def helper(timeline):
+    data = {
+      'success': 'true',
+      'surname': timeline.family.surname
+    }
+    return data
