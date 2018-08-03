e3803720e26841f2276fd3556209352838e9b720
emiliehasselmann <emiliehasselmann@berkeley.edu>
2018-08-03 16:57:35 -0400

    views/urls

--- thyme/urls.py
+++ thyme/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('homepage/', views.homepage, name='homepage'),
+    path('searchresults/', views.searchresults, name='searchresults'),
     path('homepageSearchQuery/', views.homepageSearchQuery, name='homepageSearchQuery'),
 ]
